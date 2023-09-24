7d566ee1c31d8e00dd9eeeb363ec139c895a54c5
suganya54 <144448965+suganya54@users.noreply.github.com>
2023-09-24 13:32:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-class BankAccount:
-  def initial(self,account_number,account_holder_name,initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self,amount):
-    if amount>0:
-      self.__account_balance += amount
-      print("Deposited Rs.{}. Now balance: Rs.{} ".format(amount,self.__account_balance))
-    else:  
-      print("Invalid deposit")
-
-  def withdraw(self,amount):
-    if amount>0 and amount<=self.__account_balance:
-      self.__account_balance -= amount
-      print("Withdraw Rs.{}. Now balance: Rs.{} ".format(amount,self.__account_balance))
-    else: 
-       print("Invalid withdraw or amoint insufficient")
-
-  def display(self):
-    print("Account Holder:{}. Account Number:{}. Account Balance:{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
+def linearSearchProduct(productList,targetProduct):
+  indices = []
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+      
+  return indices
 
 
-account = BankAccount()
 
-account.initial(account_number="6379697038",account_holder_name="suganthi",initial_balance=6000.0)
-account.display()
-account.deposit(2000)
-account.withdraw(1000)
-account.display()
+products = ["shoes", "boot", "Loafer","shoes", "sandal","shoes","boot"]
+target = "shoes"
+target2="apple"
+target3="boot"
+result = linearSearchProduct(products, target3)
+print(result)
